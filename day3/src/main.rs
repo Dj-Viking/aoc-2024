80592b5c73d484efda4214918416d8514e3431b5
dj-viking <anders.swedishviking@gmail.com>
2024-12-03 09:57:56 -0500

    get day3 started before work starts today

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,5 +1,12 @@
+use regex::Regex;
+
 fn main() {
-    let binding = std::fs::read_to_string("input").unwrap();
+    let binding = std::fs::read_to_string("sample").unwrap();
     let lines: Vec<&str> = binding.lines().collect();
+    let rgx = Regex::new(r"(mul\(\d{1,3},\d{1,3}\))").unwrap();
+
     println!("{:?}", lines);
+    for i in 0..lines.len() {
+
+    }
 }
