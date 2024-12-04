0aefc531b12bbc243ab330c88b47bb53fb638c49
dj-viking <anders.swedishviking@gmail.com>
2024-12-03 21:22:22 -0500

    solve part1 day3 in rusts

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,12 +1,36 @@
 use regex::Regex;
 
 fn main() {
-    let binding = std::fs::read_to_string("sample").unwrap();
+    let binding = std::fs::read_to_string("input").unwrap();
+    let mut sum = 0;
     let lines: Vec<&str> = binding.lines().collect();
-    let rgx = Regex::new(r"(mul\(\d{1,3},\d{1,3}\))").unwrap();
+    let rgx = Regex::new(r"(mul\(\d{1,3},\d{1,3}\))")
+        .unwrap();
 
-    println!("{:?}", lines);
+    let mut stuff_to_eval = vec!();
+    //println!("{:?}", lines[0]);
+    
+    let rgx2 = Regex::new(r"\d{1,3},\d{1,3}")
+        .unwrap();
+
     for i in 0..lines.len() {
+        stuff_to_eval = rgx
+            .find_iter(lines[i])
+            .map(|f| f.as_str().to_string()).collect();
+         
+        for i in 0..stuff_to_eval.len() {
+            let expr = &stuff_to_eval[i];
+            let nums: &str = rgx2.find_iter(expr)
+                .map(|f| f.as_str()).collect::<Vec<&str>>()[0];
+            let pair: Vec<i32> = nums.split(",")
+                .map(|s| s.parse::<i32>().unwrap()).collect();
 
+            //println!("pair {:?}", pair);
+            sum += pair[0] * pair[1];
+        }
     }
+    // 34806505 too low??
+    // lol it needed to be nested :)
+    // 182619815
+    println!("part 1: {}", sum);
 }
