fe7fe2caecc170d8c66e733f5e67383c14e2c590
dj-viking <anders.swedishviking@gmail.com>
2024-12-17 08:15:17 -0500

    solve part 2 day4

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,17 +1,3 @@
-// #[derive(Clone, Debug)]
-// struct Nbor {
-//     symbol: String,
-//     adj_point: (isize, isize),
-// }
-// impl Nbor {
-//     pub fn new(symbol: String, adj_point: (isize, isize)) -> Self {
-//         Self {
-//             symbol,
-//             adj_point,
-//         }
-//     }
-// }
-                              //   y  , x
 const UP: (isize, isize) =        ( 1, 0 );
 const DOWN: (isize, isize) =      (-1, 0 );
 const LEFT: (isize, isize) =      ( 0, -1);
@@ -21,12 +7,13 @@ const DOWNLEFT: (isize, isize) =  (-1, -1);
 const UPRIGHT: (isize, isize) =   ( 1, 1 );
 const DOWNRIGHT: (isize, isize) = (-1, 1 );
 
-const DIRECTIONS_STRAIGT: [(isize, isize); 4] = [
+const DIRECTIONS_STRAIGHT: [(isize, isize); 4] = [
     UP,
     DOWN,
     RIGHT,
     LEFT
 ];
+
 const DIRECTIONS_DIAG: [(isize, isize); 4] = [
     UPLEFT,
     DOWNLEFT,
@@ -52,8 +39,18 @@ fn add(a: usize, b: isize) -> Option<usize> {
     }
 }
 
+fn offset(grid: &Vec<&[u8]>, n: (usize, usize), dir: (isize, isize)) -> Option<(usize, usize)> { Some((
+    add(n.0, dir.0).filter(|n| *n < grid.len())?,
+    add(n.1, dir.1).filter(|n| *n < grid[0].len())?
+))}
+
+fn offset_check(grid: &Vec<&[u8]>, coord: (usize, usize), dir: (isize, isize), c: u8) -> bool {
+    let Some((y,x)) = offset(grid, coord, dir) else { return false; };
+    grid[y][x] == c
+}
+
 fn main() {
-    let file = std::fs::read_to_string("sample").unwrap();
+    let file = std::fs::read_to_string("input").unwrap();
     let grid = file.lines().map(str::as_bytes).collect::<Vec<&[u8]>>();
 
     println!("part1: {}", 
@@ -80,51 +77,42 @@ fn main() {
     println!("part2: {}", 
         grid.iter().enumerate() 
         .flat_map(|(y, row)| row.iter().enumerate()
-            .filter_map(move |(i, c)| (*c == b'M').then_some((y, i))))
-        .fold(0, |acc, coord|
-            acc + DIRECTIONS_STRAIGT.iter().fold(0, |acc, dir| {
-                let offset = |n: (usize, usize), dir: (isize, isize)| -> Option<(usize, usize)> { Some((
-                    add(n.0, dir.0).filter(|n| *n < grid.len())?,
-                    add(n.1, dir.1).filter(|n| *n < grid[0].len())?
-                ))};
+            .filter_map(move |(i, c)| (*c == b'A').then_some((y, i))))
+        .fold(0, |acc, coord| {
 
-                let offset_check = |n: (usize, usize), dir: (isize, isize), c: u8| -> bool {
-                    let Some((y,x)) = offset(coord, dir) else { return false; };
-                    grid[y][x] == c
-                };
+            // if there isn't 2 M's diagonally from the A in left right up or down directions
+            // then continue
+            //
+            // M M | S S | M S | S M
+            //  A  |  A  |  A  |  A
+            // S S | M M | M S | S M
+             
+            // what were the two M coords? use those to check where the S's should be
+            // opposite side of the M should be S
+            if offset_check(&grid, coord, UPRIGHT, b'M')
+            && offset_check(&grid, coord, UPLEFT, b'M')
+            && offset_check(&grid, coord, DOWNRIGHT, b'S') 
+            && offset_check(&grid, coord, DOWNLEFT, b'S')
+                { return acc + 1; }
 
-                if !offset_check(coord, (dir.0 * 2, dir.1 * 2), b'M') { return acc; }
+            if offset_check(&grid, coord, DOWNRIGHT, b'M')
+            && offset_check(&grid, coord, DOWNLEFT, b'M')
+            && offset_check(&grid, coord, UPRIGHT, b'S') 
+            && offset_check(&grid, coord, UPLEFT, b'S')
+                { return acc + 1; }
 
-                let (s1,s2) = match dir {
-                    &RIGHT => match () {
-                        _ if offset_check(coord, DOWNRIGHT, b'A') => ((-2, 0), (-2, 2)),
-                        _ if offset_check(coord, UPRIGHT, b'A') => ((2, 0), (2, 2)),
-                        _ => return acc
-                    },
-                    &LEFT => match () {
-                        _ if offset_check(coord, DOWNLEFT, b'A') => ((-2, 0), (-2, -2)), 
-                        _ if offset_check(coord, UPLEFT, b'A') => ((2, 0), (2, -2)),
-                        _ => return acc
-                    },
-                    &DOWN => match () {
-                        _ if offset_check(coord, DOWNLEFT, b'A') => ((0, -2), (-2, -2)),
-                        _ if offset_check(coord, DOWNRIGHT, b'A') => ((0, 2), (-2, 2)),
-                        _ => return acc
-                    },
-                    &UP => match () {
-                        _ if offset_check(coord, UPLEFT, b'A') => ((0, -2), (2, -2)),
-                        _ if offset_check(coord, UPRIGHT, b'A') => ((0, 2), (2, 2)),
-                        _ => return acc
-                    },
-                    _ => unreachable!(),
-                };
+            if offset_check(&grid, coord, UPLEFT, b'M')
+            && offset_check(&grid, coord, DOWNLEFT, b'M')
+            && offset_check(&grid, coord, UPRIGHT, b'S') 
+            && offset_check(&grid, coord, DOWNRIGHT, b'S')
+                { return acc + 1; }
 
-                if !offset_check(coord, s1, b'S') { return acc; }
-                if !offset_check(coord, s2, b'S') { return acc; }
-
-
-                acc + 1
-            })  
-        )
+            if offset_check(&grid, coord, UPRIGHT, b'M')
+            && offset_check(&grid, coord, DOWNRIGHT, b'M')
+            && offset_check(&grid, coord, UPLEFT, b'S') 
+            && offset_check(&grid, coord, DOWNLEFT, b'S')
+                { return acc + 1; }
+            acc 
+        })
     );
 }
