1f7850a969866082912e3efa6ac4b6275c71272c
dj-viking <anders.swedishviking@gmail.com>
2024-12-20 22:24:32 -0500

    parsing ordering rules

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -1,4 +1,16 @@
 fn main() {
     let file = std::fs::read_to_string("sample")
         .unwrap();
+
+    let ordering_rules = file.lines()
+        .filter(|l| !l.contains(',') && !l.is_empty())
+        .map(|s| 
+            s.split('|').collect::<Vec<&str>>())
+        .collect::<Vec<Vec<&str>>>();
+
+    let ordering_rules_nums = ordering_rules.into_iter()
+        .map(|x| x.into_iter().map(|n| n.parse::<usize>().unwrap()).collect::<Vec<usize>>())
+        .collect::<Vec<Vec<usize>>>();
+
+    println!("ordering rules nums {:?}", ordering_rules_nums);
 }
