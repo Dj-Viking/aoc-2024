35f8295cbe5391e84239323d799abc34a40037e0
dj-viking <anders.swedishviking@gmail.com>
2025-01-07 23:21:36 -0500

    touch up day5 part1

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -7,10 +7,6 @@ fn parse_rules_and_updates(file: &str)
         .map(|s| s.split_once('|')
             .map(|(l,r)| (l.parse::<usize>().unwrap(),r.parse::<usize>().unwrap())).unwrap())
         .collect::<Vec<(usize,usize)>>();
-        // .fold(HashMap::new(), |mut hm, (l,r)| match hm.get_mut(&l) {
-        //     None => { hm.insert(l, vec![r]); hm },
-        //     Some(e) => { e.push(r); hm }
-        // });
 
     // add each right side as a key and then the left is value for the ordering rule_nums
 
@@ -28,9 +24,10 @@ fn main() {
 
     let (rules, update_nums) = parse_rules_and_updates(&file);
 
+	// for testing
     // let update_nums = vec![vec![0,1,2,3,4,5]];
     // let rules = vec![(0,1), (1,2), (2,3), (3,4), (4,5)];
-    // 
+	//
     let matched_rules_for_num = |n| (
         rules.iter().filter(|(l,_)| *l == n).collect::<Vec<&(usize,usize)>>(),
         rules.iter().filter(|(_,r)| *r == n).collect::<Vec<&(usize,usize)>>());
@@ -49,41 +46,5 @@ fn main() {
         .sum::<usize>();
 
     println!("{:?}", answer);
-
-
-    // which one of the updates lists
-    // is already in the right order?
-    
-    // let mut updates_in_correct_order = Vec::<Vec<usize>>::new();
-
-    // for update in &update_nums {
-    //     
-    // } 
-
-    // for update in &update_nums 
-    // {
-    //     for rule_nums in &ordering_rules_nums
-    //     {
-    //         let (left,right) = (rule_nums[0], rule_nums[1]);
-    //
-    //         // if the number in the rule is not in the update list
-    //         // then go to the next update numlist
-    //         if !update.contains(&left) || !update.contains(&right) {
-    //             continue;
-    //         }
-    //
-    //         // go through all the update list nums
-    //         for n in update {
-    //             // is the n matching the left? and does it occur before the right?
-    //
-    //         }
-    //         
-    //         // println!("update {:?} \n\t\t\tfor rule -> {},{}", 
-    //         //     update, left, right);
-    //
-    //     }
-    //     println!("=======================================================");
-    //     break;
-    // }
 
 }
